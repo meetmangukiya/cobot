3054dadb21188ad3df4835d51f885771f97d5321
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-01-02 14:58:51 +0530

    cobot: Get room ids of all the rooms provided in the configuration

--- cobot.py
+++ cobot.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 # Configuration
 # -------------
 
@@ -9,3 +12,21 @@ ROOMS = [
     'coala/cobot-test'
 ]
 
+# Functionality
+# -------------
+
+def get_room_ids(rooms):
+    """
+    :pararm rooms:
+        List of room names whose room id has to be retreived.
+    :return:
+        A list of room ids.
+    """
+
+    ids = dict()
+    for room in rooms:
+        rq = requests.get("https://api.gitter.im/v1/rooms?q={room_name}".format(roon_name=room), headers={"Authorization": "Bearer {token}".format(token=GITTER_TOKEN)})
+        response = json.loads(rq.json())
+        for i in response:
+            ids[i.name] = i.id
+    return ids
